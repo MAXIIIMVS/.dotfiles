e18f44c2d43bb3fe2ac8fe0360d310e4d29fa698
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2025-09-29 08:21:48 +0330

    Fix rooter patterns.

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -21,7 +21,8 @@ return require("lazy").setup({
 			vim.g.rooter_resolve_links = true
 			vim.g.rooter_cd_cmd = "lcd"
 			vim.g.rooter_change_directory_for_non_project_files = "current"
-			vim.g.rooter_patterns = { "project.godot" }
+			vim.g.rooter_patterns =
+				{ ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "project.godot" }
 		end,
 		event = "UIEnter",
 	},
